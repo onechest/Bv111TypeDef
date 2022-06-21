b3d239a3f7ab561a69da2f4768db1f421c136cc7
BolotovYevhen <bolotov_yevgeniy@ukr.net>
2022-06-21 19:54:42 +0300

    typeDef

---
+++ Урок 21.06.2022/Урок 21.06.2022/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>     //библиотеки
+#include <string>
+
+using namespace std;
+
+using si_t = short int;
+typedef unsigned long long l_t;
+
+unsigned long long num1 = 10;
+l_t num2 = 100;
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+float sum(float a, float b)
+{
+    return a - b;
+}
+l_t sum(l_t a, l_t b)
+{
+    return a + b;
+}
+
+void main()
+{
+    setlocale(LC_ALL, "Russian"); //руссификатор
+    cout << "Привет" << endl;
+}
